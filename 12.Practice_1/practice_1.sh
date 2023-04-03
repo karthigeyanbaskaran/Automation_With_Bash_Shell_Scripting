5a1253640b0d901adcc83f377d727cf8fa369df7
VRTechnologies By NarendraP <124224897+VRTechByNarendraP@users.noreply.github.com>
2023-04-03 19:05:18 +0530

    Add files via upload

---
+++ 12.Practice_1/practice_1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+fileName=${1}
+
+if  ! [[ ${#} -eq 1 ]]  ; then 
+    echo "You Have to provide input as a file"
+    exit 
+fi 
+
+if ! [[ -e ${fileName} ]] ; then 
+    echo "Given File is not exist" ; 
+    exit
+fi
+
+if [[ -r  ${fileName} ]]  ; then 
+    cat ${fileName} 
+else 
+    echo "You Dont Have Read Permissions"
+fi 
+# [[ ${#} -eq 1 ]] || { echo "You Have to provide input as a file" ; exit ; }
+# [[ -e ${fileName} ]] || { echo "Given File is not exist" ; exit ; }
+# [[ -r  ${fileName} ]] && cat ${fileName} || echo "You Dont Have Read Permissions"
