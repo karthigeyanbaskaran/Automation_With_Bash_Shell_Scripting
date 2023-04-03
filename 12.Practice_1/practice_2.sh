140ec6e7a88e89948b33a5525f10bd6a6ba84ac4
VRTechnologies By NarendraP <124224897+VRTechByNarendraP@users.noreply.github.com>
2023-04-03 23:25:22 +0530

    Add files via upload

---
+++ 12.Practice_1/practice_2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+pkgName=${1}
+userId=$(id -u)
+
+if [[ $# -ne 1 ]] ; then 
+    echo "Please pass input as some package name "
+    exit 1
+fi 
+#  && { echo "You are not the root user. Please run this script from root" ; exit ; } 
+if [[ ${userId} -ne 0 ]] ; then 
+   echo "You are not the root user. Please run this script from root"
+   exit 2
+fi 
+if command -v ${pkgName} 1>/dev/null 2>&1 ; then 
+    echo "Your Pkg ${pkgName} is already deployed"
+else 
+    yum install ${pkgName} -y
+fi
