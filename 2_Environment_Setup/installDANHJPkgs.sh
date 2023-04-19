79008a00813d255e355d88835fa6b6d4e7745e42
VRTechnologies By NarendraP <124224897+VRTechByNarendraP@users.noreply.github.com>
2023-04-19 22:02:44 +0530

    Update installDANHJPkgs.sh

--- 2_Environment_Setup/installDANHJPkgs.sh
+++ 2_Environment_Setup/installDANHJPkgs.sh
@@ -15,22 +15,30 @@ yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce
 yum update -y 
 yum install docker-ce -y 
 
-cd /tmp/
-wget https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz
 
-tar xzf Python-3.7.9.tgz
-cd Python-3.7.9
-yum install gcc openssl-devel bzip2-devel libffi-devel -y
-./configure --enable-optimizations
-sudo make altinstall
+VERSION_ID=$(cat /etc/os-release  | grep '^VERSION_ID' |cut -d '"' -f2)
+case ${VERSION_ID} in 
+    7)
+        cd /tmp/
+        wget https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz
+        tar xzf Python-3.7.9.tgz
+        cd Python-3.7.9
+        yum install gcc openssl-devel bzip2-devel libffi-devel -y
+        ./configure --enable-optimizations
+        sudo make altinstall
+        sudo ln -sfn /usr/local/bin/python3.7 /usr/bin/python3
+        sudo ln -sfn /usr/local/bin/pip3.7 /usr/bin/pip3
+        pip3 install --upgrade pip
+        pip3 install ansible==2.9.27
+        ;; 
+    9)
+        yum install python3-pip -y
+        pip3 install --upgrade pip
+        pip3 install ansible==2.9.27
 
-sudo ln -sfn /usr/local/bin/python3.7 /usr/bin/python3
-sudo ln -sfn /usr/local/bin/pip3.7 /usr/bin/pip3
-pip3 install --upgrade pip
-pip3 install ansible==2.9.27
-
-
-
+        ;;
+    *) echo "OS Release Version is Known, so cant install ansible" ;;
+esac
 
 clear
 
